2e68fa6d3f6c2bfb423aaae034ceac7d2efdca75
soilcheng <moyancheng@gmail.com>
2015-06-27 14:24:50 +0800

    modified:   canvas/save.php

--- canvas/save.php
+++ canvas/save.php
@@ -2,10 +2,10 @@
 preg_match('#^data:image/(\w+);base64,(.+)#', $_POST['data'], $result);
 
 $type = $result[1];//图片类型
-$data = base64_decode($result[2]);//图片数据
+$data = base64_decode($result[2]);//解码图片数据
 $time = time();
 $file = "{$time}.{$type}";//图片存储名称
-file_put_contents($file, base64_decode($data));//保存图片
+file_put_contents($file, $data);//保存图片
 
 //显示图片
 header('Content-Type:image/png');
